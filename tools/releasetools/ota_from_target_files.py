8271bd7fcf03d134e3d006ca14eec47d59375feb
binkybear <binkybear@nethunter.com>
2016-11-15 21:19:03 -0600

    Add supersu flash
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -770,6 +770,12 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
 
+  # Code from Dirty Unicorns for flashing SuperSU
+  if block_based:
+    script.Print("Flashing SuperSU...")
+    common.ZipWriteStr(output_zip, "supersu/supersu.zip",
+                   ""+input_zip.read("SYSTEM/addon.d/UPDATE-SuperSU.zip"))
+    script.FlashSuperSU()
   script.ShowProgress(0.2, 10)
   device_specific.FullOTA_InstallEnd()
 
